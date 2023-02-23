6d00f5baaa90aa4e57a05edf09bb59c7e55bcb69
Tasneem Pathan <tpathan@big17.seas.upenn.edu>
2023-02-23 11:35:50 -0500

    Modified nzp bits

--- lab3-singlecycle/lc4_nzp_controller.v
+++ lab3-singlecycle/lc4_nzp_controller.v
@@ -14,7 +14,7 @@ module lc4_nzp_controller(input wire        clk,
     wire [2:0] i_nzp;
     Nbit_reg #(.n(3), .r(0)) nzp_reg(.clk(clk), .gwe(gwe), .rst(rst), .we(nzp_we), .out(o_nzp), .in(i_nzp));
 
-    assign i_nzp = wdata[15] ? 1'b100 :
-                   |wdata    ? 1'b001 :
-                               1'b010 ;
+    assign i_nzp = wdata[15] ? 3'b100 :
+                   |wdata    ? 3'b001 :
+                               3'b010 ;
 endmodule
